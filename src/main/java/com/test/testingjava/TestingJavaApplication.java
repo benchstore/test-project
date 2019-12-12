18dc8d463a94382f5255b0e2df0dc040bf9306bf
Aleksandr Prykhodko <CaH4aZzz@ua.fm>
2019-12-12 16:52:06 +0200

    init commit

---
+++ src/main/java/com/test/testingjava/TestingJavaApplication.java
@@ -0,0 +1,13 @@
+package com.test.testingjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestingJavaApplication.class, args);
+    }
+
+}
